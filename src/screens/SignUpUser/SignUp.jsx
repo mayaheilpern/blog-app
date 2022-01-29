ed75dbee7ecb9b70aa41d08abf036ec904087e39
Michael Abebe <michaelabebe@michaels-mbp.myfiosgateway.com>
2022-01-29 15:32:32 -0500

    Sign up User

--- src/screens/SignUpUser/SignUp.jsx
+++ src/screens/SignUpUser/SignUp.jsx
@@ -12,11 +12,8 @@ const default_User = {
 
 const SignUpUser = () => {
   const [input, setInput] = useState(default_User);
-  const [firstName, setFirstName]=useState("");
-  const [lastName, setLastName]=useState("");
-  const [userName, setUserName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("")
+
+
   const handleTextInput = (e) => {
     const { name, value } = e.target;
     setInput((prevInput) => ({
@@ -24,26 +21,26 @@ const SignUpUser = () => {
       [name]: value,
     }));
   }
-  const handleInput = async (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const fields = input;
     const res = await apiPost("/", fields);
     console.log(res);
     setInput(default_User);
   }
-  return <form onSubmit={handleInput}>
+  return <form onSubmit={handleSubmit}>
     <label>user Name</label>
-    <input name={"userName"} value={userName} onChange={e => { handleTextInput }}></input>
+    <input name="userName" value={input.userName} onChange={e => { handleTextInput() }}></input>
     <label>first Name</label>
-      <input name={"firstName"} value={firstName} onChange={e => { handleTextInput }}></input>
+      <input name="firstName" value={input.firstName} onChange={e => { handleTextInput() }}></input>
     <label>last Name</label>
-      <input  name={"lastName"} value={lastName} onChange={e => { handleTextInput }}></input>
+      <input  name="lastName" value={input.lastName} onChange={e => { handleTextInput() }}></input>
     <label>Email</label>
-      <input  name={"email"} value={email} onChange={e => { handleTextInput }}></input>
+      <input  name="email" value={input.email} onChange={e => { handleTextInput() }}></input>
     <label>password</label>
-      <input name={"password"} value={password} onChange={e => { handleTextInput }}></input>
+      <input name="password" value={input.password} onChange={e => { handleTextInput() }}></input>
     <label>confirmPassword</label>
-      <input  name={"confirmPassword"} onChange={e => { handleTextInput }}></input>
+    <input name="confirmPassword" value={input.confirmPassword} onChange={e => { handleTextInput() }}></input>
   </form>;
 };
 
