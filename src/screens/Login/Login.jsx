98045d81cb7dfcb0d8b7c4f16885e59648059a4c
Jacob Dye <jacobdye@pop-os.localdomain>
2022-01-30 12:05:18 -0500

    added forms and made the signup form password types

--- src/screens/Login/Login.jsx
+++ src/screens/Login/Login.jsx
@@ -1,5 +1,23 @@
+import { useState } from "react";
+import './login.css'
+
+
 const Login = () => {
-  return <div></div>;
+  return(
+    <div>
+      <form>
+        <fieldset>
+          <legend> Login</legend>
+          <label>Username</label>
+          <input type="text" />
+          <br />
+          <label>Password</label>
+          <input type="password" />
+        </fieldset>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  )
 };
 
 export default Login;
