3f82b801bc387d74da54d404ceb8abbce9d60a3d
Jacob Dye <jacobdye@pop-os.localdomain>
2022-01-30 14:57:45 -0500

    Working on User Page

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import DisplayBlog from "./screens/DisplayBlog/DisplayBlog";
 import Login from "./screens/Login/Login";
 import Home from "./screens/Home/Home";
 import SignUpUser from "./screens/SignUpUser/SignUp";
+import User from "./screens/User/User";
 
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<DisplayBlog />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUpUser />} />
-        <Route path="/:id" element={<DisplayBlog />} />
+        <Route path="/user/:id" element={<User />} />
       </Routes>
     </div>
   );
