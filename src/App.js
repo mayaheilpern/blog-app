bb2c5f324f8dcaddc9163afe0e084c0c95931264
Trung Diec <tdiec00@gmail.com>
2022-01-31 14:36:44 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -1,33 +1,23 @@
-
-
-
 import {Routes, Route} from "react-router-dom";
-
-
-import Layout from "./Layout/Layout";
 import DisplayBlog from "./screens/DisplayBlog/DisplayBlog";
 import Home from "./screens/Home/Home";
 import Login from "./screens/Login/Login";
 import Logout from "./screens/Logout/Logout";
-// import Home from "./screens/Home/Home";
 import SignUpUser from "./screens/SignUpUser/SignUp";
 import User from "./screens/User/User";
 import AddBlog from "./screens/AddBlog/AddBlog";
 
 function App() {
-
   return (
     <div className="App">
       <Routes>
-
         <Route path="/" element={<Home />} />
-        <Route path='/logout' element={<Logout />} />
+        <Route path="/logout" element={<Logout />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUpUser  />} />
+        <Route path="/signup" element={<SignUpUser />} />
         <Route path="/user/:id/info" element={<User />} />
-        <Route path="/displayall" element={<DisplayBlog />}/>
-        <Route path="/addblog" element={<AddBlog />} />          
-
+        <Route path="/displayall" element={<DisplayBlog />} />
+        <Route path="/addblog" element={<AddBlog />} />
       </Routes>
     </div>
   );
