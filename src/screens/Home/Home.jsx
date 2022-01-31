292d12c30947eabda05f359777a94e0f6e1bb875
Jacob Dye <jacobdye@pop-os.localdomain>
2022-01-31 10:08:21 -0500

    new homepage

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -1,8 +1,12 @@
 import Layout from "../../Layout/Layout";
+import './home.css'
 const Home = () => {
   return (
     <Layout>
-      <div></div>
+      <div className="homeDiv">
+      <h1 id="welcome">A new Blogger has Appeared!</h1>
+      <img src="https://media3.giphy.com/media/4c0sdgYcz9mpO/giphy.gif?cid=ecf05e472fxiu8209dp4r4i971767rvmxlore0kqh1q4090l&rid=giphy.gif&ct=g" alt="Welcome" id="helloThere" />
+      </div>
     </Layout>
   );
 };
--- src/screens/User/User.jsx
+++ src/screens/User/User.jsx
@@ -24,7 +24,7 @@ export default function User() {
     return( 
     <div> 
         <UserLayout id={id}>
-            <div className="card-container">
+            <div className="card-container2">
                 {posts?.posts?.posts.map((post) => {
                         return (
                             <div key={post._id} className="post-cards">
