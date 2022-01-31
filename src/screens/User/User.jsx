39ef925eebfb3ec094f6236e3987134a069de801
Trung Diec <tdiec00@gmail.com>
2022-01-31 12:14:33 -0500

    add blog works

--- src/screens/User/User.jsx
+++ src/screens/User/User.jsx
@@ -1,43 +1,39 @@
-import { useParams } from "react-router-dom";
+import {useParams} from "react-router-dom";
 // import Posts from "../../components/Posts/Posts";
 import Layout from "../../Layout/Layout";
-import { useEffect, useState } from "react";
-import { getUserPosts } from "../../services/apiConfig";
-import './User.css'
-
+import {useEffect, useState} from "react";
+import {getUserPosts} from "../../services/apiConfig";
+import "./User.css";
 
 export default function User() {
   const [posts, setPosts] = useState([]);
-  const { id } = useParams();
+  const {id} = useParams();
   console.log(localStorage.getItem("id"));
-    useEffect(() => {
-        const fetchPosts = async () => {
-            // console.log(id)
-            const allPosts = await getUserPosts(`/${id}/posts`);
-            setPosts(allPosts.data)
-            console.log(allPosts.data)
-        }
-        fetchPosts()
-    }, [id])
-    // console.log(posts)
+  useEffect(() => {
+    const fetchPosts = async () => {
+      // console.log(id)
+      const allPosts = await getUserPosts(`/${id}/posts`);
+      setPosts(allPosts.data);
+      console.log(allPosts.data);
+    };
+    fetchPosts();
+  }, [id]);
+  // console.log(posts)
 
-    return( 
-    <div> 
-
-        <UserLayout id={id}>
-            <div className="card-container2">
-
-                {posts?.posts?.posts.map((post) => {
-                        return (
-                            <div key={post._id} className="post-cards">
-                                <h3>{`${posts.posts.userName}`}</h3>
-                                <p>{post.content}</p>
-                            </div>
-                        )
-                    })
-                }
-            </div>
-        </Layout>
-    </div> 
-    );
+  return (
+    <div>
+      <Layout id={id}>
+        <div className="card-container2">
+          {posts?.posts?.posts.map((post) => {
+            return (
+              <div key={post._id} className="post-cards">
+                <h3>{`${posts.posts.userName}`}</h3>
+                <p>{post.content}</p>
+              </div>
+            );
+          })}
+        </div>
+      </Layout>
+    </div>
+  );
 }
